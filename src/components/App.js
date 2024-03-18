63f6f462bf343d497c01e7633660ba3a2b9ed095
Polina <smivasilisa@gmail.com>
2024-03-18 15:48:14 +0100

    fixed App path in index

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import UserList from "./UserList/UserList";
 import TodoList from "./TodoList/TodoList";
 import UserCard from "./UserList/UserCard";
 
-// class App extends React.Component {
+// class App extends React.Component { /// was on video (065) React.Formy
 //   render() {
 //     return <UserList />
 //   }
